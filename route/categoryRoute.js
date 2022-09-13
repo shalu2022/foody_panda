a323f994423d0a892a7d0b208b20221fb88bc4b6
shalu2022 <frontendstuff2022@gmail.com>
2022-09-13 17:34:33 +0530

    change in categoryRouter

--- route/categoryRoute.js
+++ route/categoryRoute.js
@@ -1,5 +1,5 @@
 const route = require('express').Router();
-const CategoryCtrl = require(`../controller/categoryCtrl`)
+const CategoryCtrl = require(`../controller/CategoryCtrl`)
 const auth = require(`../middleware/auth`)
 const adminAuth = require(`../middleware/AdminAuth`)
 
